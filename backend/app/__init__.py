d293b51e7a193bbad7741f91f7a11831e9ae110c
Seu Nome <joni_gomes@hotmail.com>
2025-02-07 17:21:06 -0300

    [fix] - Adjusting CORS on routes

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
+    CORS(app, resources={r"/*": {"origins": "http://localhost:8080"}})
     app.config['UPLOAD_FOLDER'] = './uploads'
     app.config['ZIP_FOLDER'] = './zips'
     
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,16 +1,13 @@
-from flask import Flask, Blueprint, request, send_from_directory, jsonify
-from flask_cors import CORS
+from flask import Flask, Blueprint, request, send_from_directory, jsonify, current_app
+from flask_cors import cross_origin
 import os
 from werkzeug.utils import secure_filename
 from .video_processing import extract_frames, create_zip
 
-app = Flask(__name__)
-
-CORS(app, origins=["http://localhost:8080", "http://localhost:5000"])
-
 bp = Blueprint('routes', __name__)
 
 @bp.route('/upload', methods=['POST'])
+@cross_origin()
 def upload_video():
     if 'file' not in request.files:
         return jsonify({"error": "No file part"}), 400
@@ -37,5 +34,13 @@ def upload_video():
         })
 
 @bp.route('/download/<zip_filename>', methods=['GET'])
+@cross_origin()
 def download_zip(zip_filename):
-    return send_from_directory('./zips', zip_filename)
+    zip_folder = os.path.join(os.getcwd(), 'zips')
+    file_path = os.path.join(zip_folder, zip_filename)
+
+    if os.path.exists(file_path):
+        return send_from_directory(zip_folder, zip_filename)
+    else:
+        current_app.logger.error(f"Arquivo {zip_filename} não encontrado.")
+        return jsonify({"error": "Arquivo não encontrado"}), 404
